22e254a351c1153fa523a8bc516958566c04cec3
Simon Roussel <sim.roussel@gmail.com>
2016-11-22 21:35:59 +0100

    resolve conflicts

--- app/components/games/flappybird/flappybird.controller.js
+++ app/components/games/flappybird/flappybird.controller.js
@@ -1,68 +1,78 @@
- (function () {
-   'use strict'
+ (function() {
+     'use strict'
 
-   angular
-    .module('app.games')
-    .controller('flappybirdCtrl', flappybirdCtrl)
+     angular
+         .module('app.games')
+         .controller('flappybirdCtrl', flappybirdCtrl)
 
-   flappybirdCtrl.$inject = ['$scope', 'GameArea', 'GamePiece']
+     flappybirdCtrl.$inject = ['$scope', 'GameArea', 'GamePiece']
 
-   function flappybirdCtrl ($scope, GameArea) {
-      // subscribe to any keydown/keyup events (@see index.html)
-     $scope.$on('keydown-evt', function (targetScope, evt) {
-       console.log('A key was pressed by an user:', evt.keyCode)
-     })
-     $scope.$on('keyup-evt', function (targetScope, evt) {
-       console.log('A key was released by an user:', evt.keyCode)
-     })
+     function flappybirdCtrl($scope, GameArea) {
+         // subscribe to any keydown/keyup events (@see index.html)
+         $scope.$on('keydown-evt', function(targetScope, evt) {
+             if (evt.keyCode === 38) {
+                 moveup();
+             }
+             if (evt.keyCode === 40) {
+                 movedown()
+             }
+             if (evt.keyCode === 39) {
+                 clearmove();
+                 moveright();
+             }
+             if (evt.keyCode === 37) {
+                 clearmove();
+                 moveleft();
+             }
+         })
 
-      // private vars
-     var game = null
-     var canvasContainer = angular.element(document.getElementById('canvascontainer'))
+         // private vars
+         var game = null
+         var canvasContainer = angular.element(document.getElementById('canvascontainer'))
 
-      // scoped vars
-     var vm = this
-     vm.infos = {
-       isRunning: false,
-       score: 0
+         // scoped vars
+         var vm = this
+         vm.infos = {
+             isRunning: false,
+             score: 0
+         }
+         vm.restartGame = restartGame
+
+         // run
+         startGame()
+
+         /** @method restartGame */
+         function restartGame() {
+             canvasContainer.empty()
+             game.stop()
+             game.clear()
+             startGame()
+         }
+
+         function startGame() {
+             game = new GameArea(canvasContainer, vm.infos)
+             game.start()
+         }
+
+         function moveup(e) {
+             game.bird.speedY = -1
+         }
+
+         function movedown() {
+             game.bird.speedY = 1
+         }
+
+         function moveleft() {
+             game.bird.speedX = -1
+         }
+
+         function moveright() {
+             game.bird.speedX = 1
+         }
+
+         function clearmove(e) {
+             game.bird.speedX = 0
+             game.bird.speedY = 0
+         }
      }
-     vm.restartGame = restartGame
-
-      // run
-     startGame()
-
-      /** @method restartGame */
-     function restartGame () {
-       canvasContainer.empty()
-       game.stop()
-       game.clear()
-       startGame()
-     }
-
-     function startGame () {
-       game = new GameArea(canvasContainer, vm.infos)
-       game.start()
-     }
-
-     function moveup (e) {
-       game.bird.speedY = -1
-     }
-
-     function movedown () {
-       game.bird.speedY = 1
-     }
-
-     function moveleft () {
-       game.bird.speedX = -1
-     }
-
-     function moveright () {
-       game.bird.speedX = 1
-     }
-
-     function clearmove (e) {
-       game.bird.speedX = 0
-       game.bird.speedY = 0
-     }
-   }
  })()
